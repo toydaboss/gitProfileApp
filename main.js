c566866ba2f732770c13e3188a02c1cea0776595
Aderibigbe Toyese <43404546+toydaboss@users.noreply.github.com>
2020-07-05 12:00:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const unContainer = document.querySelector(".result-username");
 const reposContainer = document.querySelector(".result-repos");
 const urlContainer = document.querySelector(".result-url");
 const avatar = document.querySelector(".result-avatar");
-const resultText = document.querySelector(".result-text");
+const resultText = document.querySelector(".result");
 
 const client_id = " Iv1.cc4d493854730f6b";
 const client_secret = "de3d550fb4228e770096c3c81d64b3161ed66f78";
@@ -18,7 +18,6 @@ const fetchUsers = async(user) => {
 };
 const showData =()=>{
     fetchUsers(inputValue.value).then((res)=>{
-        console.log(res);
         nameContainer.innerHTML=`<b>Name:</b> <span class="result-name">${res.data.name}</span>`;
         unContainer.innerHTML= `<b>Username:</b> <span class="result-username">${res.data.login}</span>`;
         reposContainer.innerHTML= `<b>Repos:</b> <span class="result-repos">${res.data.public_repos}</span>`;
@@ -26,8 +25,13 @@ const showData =()=>{
         avatar.src=`${res.data.avatar_url}`;
     });
 };
-searchButton.addEventListener('click',() =>{
+searchButton.addEventListener('click',(e) =>{
+    e.preventDefault();
     showData();
-   resultText.style.display = "grid" ;
-   avatar.style.display = "unset";
+   resultText.style.display = "flex" ;
+});
+inputValue.addEventListener('keyup',(e) =>{
+    e.preventDefault();
+    showData();
+   resultText.style.display = "flex" ;
 });
